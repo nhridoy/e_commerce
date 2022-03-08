38bb4260cc7dfd976261794717cc455913e18b67
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-08 21:55:02 +0600

    api modified, product option and wishlist and cart button added on homepage modal

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from productapp import models
 
+class ProductColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Colors
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     company_name = serializers.CharField(source='company_name.company_name')
+    product_colors = ProductColorSerializer(many=True)
+    product_sizes = serializers.StringRelatedField(many=True)
     total_rating_count = serializers.IntegerField()
     avg_rating = serializers.IntegerField()
     class Meta:
